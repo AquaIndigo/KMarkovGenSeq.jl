feba5cc890301cdbadc69c911cb249e722593c69
Documenter.jl <documenter@juliadocs.github.io>
2021-05-06 13:18:02 +0000

    build based on 588f0e3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = KMarkovGenSeq","category":"page"},{"location":"#KMarkovGenSeq","page":"Home","title":"KMarkovGenSeq","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [KMarkovGenSeq]","category":"page"}]
+}
